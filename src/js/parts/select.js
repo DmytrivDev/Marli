e8ba7d994e386b6ae628307a1471edbceee965a4
Uzurch1k <galcenkovlad7@gmail.com>
2024-06-17 14:50:33 +0300

    fix

--- src/js/parts/select.js
+++ src/js/parts/select.js
@@ -1,14 +1,13 @@
 import NiceSelect2 from 'nice-select2';
-// import 'nice-select2/dist/css/nice-select2.css';
 
 new NiceSelect2(document.getElementById('a-select'), {
   searchable: false,
   placeholder: 'AT',
 });
 
-const loadingBox = document.querySelector('.loading-cl__loading-box');
-const loadingName = document.querySelector('.loading-cl__loading-address');
-const loadingList = document.querySelector('.loading-cl__loading-list');
+const loadingBox = document.querySelector('.choice-address');
+const loadingName = document.querySelector('.choice-address__input');
+const loadingList = document.querySelector('.choice-address__list');
 
 if (loadingBox) {
   loadingName?.addEventListener('click', () => {
@@ -17,7 +16,7 @@ if (loadingBox) {
   });
 
   window.addEventListener('click', e => {
-    if (!e.target.closest('.loading-cl__loading-address')) {
+    if (!e.target.closest('.choice-address__input')) {
       loadingList?.classList.remove('is-opened');
       loadingName?.classList.remove('is-opened');
     }

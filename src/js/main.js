4878694f01c10553644db111869a6d5c86670ed5
Uzurch1k <galcenkovlad7@gmail.com>
2024-06-17 22:47:49 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { openBurger } from './parts/navigation';
 import { instFeedbackSlider } from './parts/splide';
 import './parts/select';
+import './parts/flatpickr';
 
 openBurger();
 instFeedbackSlider();
- 
---
+++ src/js/parts/flatpickr.js
@@ -0,0 +1,41 @@
+import flatpickr from 'flatpickr';
+import { French } from 'flatpickr/dist/l10n/fr.js';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+flatpickr('#choose-date', {
+  locale: French,
+  altInput: true,
+  altFormat: 'M j, Y',
+  dateFormat: 'd.m.Y',
+  showMonths: 1,
+  minDate: 'today',
+  shorthandCurrentMonth: true,
+  maxDate: new Date().fp_incr(21),
+  // inline: true,
+});
+
+const style = document.createElement('style');
+style.innerHTML = `
+    .flatpickr-calendar .dayContainer span:nth-child(n+36) {
+      display: none !important;
+    }
+  `;
+document.head.appendChild(style);
+
+const chooseDate = document.querySelector('.praci-cl__list-choose');
+const radioButtons = document.querySelectorAll('input[name="praci-cl-date"]');
+
+chooseDate?.addEventListener('click', () => {
+  chooseDate.classList.add('is-check');
+
+  radioButtons.forEach(function (radio) {
+    radio.checked = false;
+  });
+});
+
+radioButtons?.forEach(radio => {
+  radio.addEventListener('click', () => {
+    chooseDate.classList.remove('is-check');
+  });
+});

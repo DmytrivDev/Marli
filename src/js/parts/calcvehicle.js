05f70c18bb669677130e5b612298c335f49460ff
Uzurch1k <galcenkovlad7@gmail.com>
2024-06-18 23:51:31 +0300

    fix

--- src/js/parts/calcvehicle.js
+++ src/js/parts/calcvehicle.js
@@ -1,45 +1,61 @@
-const boxLabels = document.querySelectorAll('.vehicle__content-label');
+const handleSelection = selectedRadio => {
+  const labels = document.querySelectorAll('.vehicle__content-label');
 
-boxLabels.forEach(label => {
-  label.addEventListener('click', () => {
-    boxLabels.forEach(lab => {
-      lab.classList.remove('is-check');
+  labels.forEach(labl => {
+    labl.classList.remove('is-check');
 
-      const backview = lab.querySelector('.vehicle__backview');
-      if (backview) {
-        backview.classList.remove('is-check');
-      }
+    const backview = labl.querySelector('.vehicle__backview');
+    if (backview) {
+      backview.classList.remove('is-check');
+    }
 
-      const previewImg = lab.querySelector('.vehicle__preview-img');
-      if (previewImg) {
-        previewImg.classList.remove('is-hidden');
-      }
+    const previewImg = labl.querySelector('.vehicle__preview-img');
+    if (previewImg) {
+      previewImg.classList.remove('is-hidden');
+    }
 
-      const sidebar = lab
-        .closest('.vehicle__item')
-        .querySelector('.vehicle__sidebar');
-      if (sidebar) {
-        sidebar.classList.remove('is-check');
-      }
-    });
+    const sidebar = labl
+      .closest('.vehicle__item')
+      .querySelector('.vehicle__sidebar');
+    if (sidebar) {
+      sidebar.classList.remove('is-check');
+    }
+  });
 
-    label.classList.add('is-check');
+  const selectedLabel = selectedRadio.closest('.vehicle__content-label');
 
-    const backview = label.querySelector('.vehicle__backview');
+  if (selectedLabel) {
+    selectedLabel.classList.add('is-check');
+
+    const backview = selectedLabel.querySelector('.vehicle__backview');
     if (backview) {
       backview.classList.add('is-check');
     }
 
-    const previewImg = label.querySelector('.vehicle__preview-img');
+    const previewImg = selectedLabel.querySelector('.vehicle__preview-img');
     if (previewImg) {
       previewImg.classList.add('is-hidden');
     }
 
-    const sidebar = label
+    const sidebar = selectedLabel
       .closest('.vehicle__item')
       .querySelector('.vehicle__sidebar');
     if (sidebar) {
       sidebar.classList.add('is-check');
+    }
+  }
+};
+
+const radioButtons = document.querySelectorAll('input[name="vehicle-truck"]');
+
+radioButtons.forEach(radio => {
+  if (radio.checked) {
+    handleSelection(radio);
+  }
+
+  radio.addEventListener('change', () => {
+    if (radio.checked) {
+      handleSelection(radio);
     }
   });
 });

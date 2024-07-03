3d1866353496b757e3b66791acf248836f057e80
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-07-03 12:24:28 +0300

    Changes

--- src/js/parts/navigation.js
+++ src/js/parts/navigation.js
@@ -1,3 +1,5 @@
+import scrollToElement from 'scroll-to-element';
+
 const burger = document.querySelector('.header__burger');
 const mobMenu = document.querySelector('.mob-menu');
 const body = document.querySelector('body');
@@ -31,3 +33,34 @@ window.addEventListener('click', () => {
     openedMenu?.classList.remove('is-opened');
   });
 });
+
+const ankors = document.querySelectorAll('.ankor > a, a.ankor');
+if (ankors) {
+  ankors.forEach(el => {
+    el.addEventListener('click', e => {
+      e.preventDefault();
+      const targ = e.target.getAttribute('href');
+      const targetH = document.querySelector(targ);
+      console.log(targetH)
+      if (targ && targetH) {
+        const options = {
+          offset: 0,
+          duration: 700,
+        };
+
+        scrollToElement(targetH, options);
+      } else {
+        const protocol = window.location.protocol;
+        const host = document.location.host;
+        const fullUrl = window.location.href;
+        const languageCodeRegex = /\/([a-z]{2})\//;
+        const match = fullUrl.match(languageCodeRegex);
+        const languageCode = match ? `/${match[1]}` : '';
+
+        const url = protocol + '//' + host + languageCode + targ;
+
+        window.location.href = url;
+      }
+    });
+  });
+}
